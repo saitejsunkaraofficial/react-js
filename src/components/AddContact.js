9159f85312e386c9281df8c883c89f8c4c260005
saitejsunkaraofficial <saitej.sunkara.official@gmail.com>
2021-07-01 02:44:13 +0530

    useState Hooks Learnt

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 
 export default class AddContact extends Component {
+    state = {
+        name:"",
+        email:""
+    }
+    add = (e) => {
+        e.preventDefault();
+        if(this.state.name===""||this.state.email==="") {
+            alert("All fields are mandatory.");
+        }
+        console.log(this.state.name, this.state.email);
+    }
     render() {
         return (
             <div className="ui main">
                 <h2>Add Contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                     <label>Name</label>
-                    <input type="text" name="name" placeholder="Enter name"></input>
+                    <input type="text" name="name" placeholder="Enter name" value={this.state.name} onChange={(e)=>this.setState({name:e.target.value})} autoComplete="false" autoFocus={true} />
                     </div>
                     <div className="field">
                     <label>Email</label>
-                    <input type="email" name="name" placeholder="Enter Email Address"></input>
+                    <input type="email" name="name" placeholder="Enter Email Address" value={this.state.email} onChange={(e)=>this.setState({email:e.target.value})} autoComplete="false" />
                     </div>
                     <button className="ui button blue">Add</button>
                 </form>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,19 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ShowContacts from './ShowContacts';
 
 const ContactList = () => {
-    const contacts = [
-        {
-            "name":"Sai Tej",
-            "email":"saitej@gmail.com",
-            id:1
-        },
-        {
-            "name":"Sai Chand",
-            "email":"saichand@gmail.com",
-            id:2
-        }
-    ];
+    const [contacts, setcontacts] = useState([]);
     return (
         <div style={{marginTop:'20px'}}>
             <ShowContacts contacts={contacts} />
